771fdb39952ce156024b529d7a1fa936862589a1
Tiago Pala <tiago_pala@outlook.com>
2021-11-12 16:49:39 -0300

    Implementação do Asp.Net Identity e AuthController

---
+++ AspNetCoreEnterpriseApp/src/services/EnterpriseApp.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using EnterpriseApp.Identidade.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EnterpriseApp.Identidade.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] UserRegisterDTO user)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var identityUser = new IdentityUser
+            {
+                UserName = user.Email,
+                Email = user.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(identityUser, user.Password);
+
+            if (!result.Succeeded) return BadRequest();
+
+            await _signInManager.SignInAsync(identityUser, false);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UserLoginDTO user)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(user.Email, user.Password, false, true);
+
+            if (!result.Succeeded) return BadRequest();
+
+            return Ok();
+        }
+    }
+}
--- AspNetCoreEnterpriseApp/src/services/EnterpriseApp.Identidade.API/Startup.cs
+++ AspNetCoreEnterpriseApp/src/services/EnterpriseApp.Identidade.API/Startup.cs
@@ -21,6 +21,8 @@ namespace EnterpriseApp.Identidade.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(options => options.LowercaseUrls = true);
+
             // Configuração do ApplicationDbContext
             services.AddDbContext<ApplicationDbContext>(options =>
             {
@@ -33,21 +35,24 @@ namespace EnterpriseApp.Identidade.API
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EnterpriseApp.Identidade.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "EnterpriseApp Identity API",
+                    Description = "This API is responsible for taking care of user authentication e authorization services.",
+                    Contact = new OpenApiContact { Name = "Tiago Pala", Email = "tiago_pala@outlook.com"},
+                    License = new OpenApiLicense { Name = "MIT", Url = new System.Uri("https://opensource.org/licenses/MIT")},
+                    Version = "v1" });
             });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"));
+
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EnterpriseApp.Identidade.API v1"));
-            }
 
             app.UseHttpsRedirection();
 
