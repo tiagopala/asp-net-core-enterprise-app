49729ef685540709460ee0ed2fe53463a8ecc370
Tiago Pala <tiago_pala@outlook.com>
2021-11-29 13:55:07 -0300

    Arrumando css mensagens de erros

--- src/web/EnterpriseApp.WebApp.MVC/Controllers/HomeController.cs
+++ src/web/EnterpriseApp.WebApp.MVC/Controllers/HomeController.cs
@@ -18,19 +18,19 @@ namespace EnterpriseApp.WebApp.MVC.Controllers
             if (statusCode == 500)
             {
                 error.Title = "An Error occurred!";
-                error.Message = "An Error occurred! Try again later.";
+                error.Message = "An Error occurred! Try again later";
                 error.ErrorCode = statusCode;
             }
             else if (statusCode == 404)
             {
                 error.Title = "Oops! Page not found.";
-                error.Message = "The page you are looking for does not exist.";
+                error.Message = "The page you are looking for does not exist";
                 error.ErrorCode = statusCode;
             }
             else if (statusCode == 403)
             {
                 error.Title = "Unauthorized";
-                error.Message = "You do not have permission to do this.";
+                error.Message = "You do not have permission to do this";
                 error.ErrorCode = statusCode;
             }
             else
