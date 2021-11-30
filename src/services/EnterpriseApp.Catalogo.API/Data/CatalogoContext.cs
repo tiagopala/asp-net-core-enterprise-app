dc5b8d73bd5a0825a20dc574e0a962aba04924d3
Tiago Pala <tiago_pala@outlook.com>
2021-11-30 09:04:04 -0300

    Criação DbContext Catalogo

---
+++ src/services/EnterpriseApp.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,13 @@
+﻿using EnterpriseApp.Catalogo.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnterpriseApp.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) {}
+
+        // DbSets
+        public DbSet<Produto> Produtos => Set<Produto>();
+    }
+}
