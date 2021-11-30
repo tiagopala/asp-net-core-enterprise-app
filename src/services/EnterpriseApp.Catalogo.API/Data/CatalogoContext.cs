2431231222d2ea51c30d011913b6c962deb40d6d
Tiago Pala <tiago_pala@outlook.com>
2021-11-30 11:39:29 -0300

    Vínculo mappings - DbContext - migrations

--- src/services/EnterpriseApp.Catalogo.API/Data/CatalogoContext.cs
+++ src/services/EnterpriseApp.Catalogo.API/Data/CatalogoContext.cs
@@ -9,5 +9,10 @@ namespace EnterpriseApp.Catalogo.API.Data
 
         // DbSets
         public DbSet<Produto> Produtos => Set<Produto>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
     }
 }
--- src/services/EnterpriseApp.Catalogo.API/Startup.cs
+++ src/services/EnterpriseApp.Catalogo.API/Startup.cs
@@ -1,16 +1,11 @@
+using EnterpriseApp.Catalogo.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EnterpriseApp.Catalogo.API
 {
@@ -26,6 +21,8 @@ namespace EnterpriseApp.Catalogo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CatalogoContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
