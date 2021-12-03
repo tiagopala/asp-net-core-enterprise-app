34dc168ff08b6e64d4a541fdcae23a66113708f1
Tiago Pala <tiago_pala@outlook.com>
2021-12-03 10:07:56 -0300

    Criação de um AsyncRetryPolice customizado para as chamadas na API de catalogo

--- src/web/EnterpriseApp.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/EnterpriseApp.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,9 +1,11 @@
-﻿using EnterpriseApp.WebApp.MVC.Services;
+﻿using EnterpriseApp.WebApp.MVC.Extensions;
+using EnterpriseApp.WebApp.MVC.Services;
 using EnterpriseApp.WebApp.MVC.Services.Handlers;
 using EnterpriseApp.WebApp.MVC.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Refit;
 using System;
 
 namespace EnterpriseApp.WebApp.MVC.Configuration
@@ -19,16 +21,12 @@ namespace EnterpriseApp.WebApp.MVC.Configuration
                 configure.BaseAddress = new Uri(configuration.GetSection("AuthAPI").Get<AuthAPIConfig>().Endpoint);
             });
 
-            //services.AddHttpClient<ICatalogService, CatalogService>(configure => 
-            //{
-            //    configure.BaseAddress = new Uri(configuration.GetSection("CatalogAPI").Get<CatalogApiConfig>().Endpoint);
-            //}).AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
-
             services.AddHttpClient("CatalogRefit", configure =>
             {
                 configure.BaseAddress = new Uri(configuration.GetSection("CatalogAPI").Get<CatalogApiConfig>().Endpoint);
             }).AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-              .AddTypedClient(Refit.RestService.For<ICatalogServiceRefit>);
+              .AddTypedClient(RestService.For<ICatalogServiceRefit>)
+              .AddPolicyHandler(HttpCustomPolicyExtensions.GetHttpErrorWaitAndRetryCustomPolicy());
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
