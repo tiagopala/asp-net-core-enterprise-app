18e252a60e7ba0375540c9b6ce96f48754cd93ee
Tiago Pala <tiago_pala@outlook.com>
2021-12-07 23:50:04 -0300

    Inicio implementação mappings da entidade/tabela Customer

---
+++ src/services/EnterpriseApp.Cliente.API/Data/Mappings/CustomerMapping.cs
@@ -0,0 +1,17 @@
+﻿using EnterpriseApp.Cliente.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EnterpriseApp.Cliente.API.Data.Mappings
+{
+    public class CustomerMapping : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.Property(c => c.Name)
+                .IsRequired();
+
+            builder.ToTable("Customers");
+        }
+    }
+}
