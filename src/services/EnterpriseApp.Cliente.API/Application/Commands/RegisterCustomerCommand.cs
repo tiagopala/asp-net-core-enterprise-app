c8e08e88122a880b3a69530a9c33cd0f9334c4e8
Tiago Pala <tiago_pala@outlook.com>
2021-12-09 18:51:39 -0300

    Criação comando para registrar cliente

---
+++ src/services/EnterpriseApp.Cliente.API/Application/Commands/RegisterCustomerCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnterpriseApp.Cliente.API.Application.Commands
+{
+    public class RegisterCustomerCommand
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+    }
+}
--- src/services/EnterpriseApp.Cliente.API/Data/CustomerContext.cs
+++ src/services/EnterpriseApp.Cliente.API/Data/CustomerContext.cs
@@ -1,10 +1,12 @@
 ﻿using EnterpriseApp.Cliente.API.Models;
+using EnterpriseApp.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace EnterpriseApp.Cliente.API.Data
 {
-    public class CustomerContext : DbContext
+    public class CustomerContext : DbContext, IUnitOfWork
     {
         public CustomerContext(DbContextOptions options) : base(options)
         {
@@ -15,6 +17,7 @@ namespace EnterpriseApp.Cliente.API.Data
         // DbSets
         public DbSet<Customer> Customers => Set<Customer>();
         public DbSet<Address> Addresses => Set<Address>();
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
@@ -22,5 +25,8 @@ namespace EnterpriseApp.Cliente.API.Data
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomerContext).Assembly);
         }
+
+        public async Task<bool> Commit()
+            => await base.SaveChangesAsync() > 0;
     }
 }
