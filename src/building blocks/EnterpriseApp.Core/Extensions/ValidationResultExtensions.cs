d1298869bcb85210c439f81e85a9042515fdef63
Tiago Pala <tiago_pala@outlook.com>
2021-12-13 15:13:48 -0300

    Criação da classe de extensão do Fluent Validation Result

---
+++ src/building blocks/EnterpriseApp.Core/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation.Results;
+
+namespace EnterpriseApp.Core.Extensions
+{
+    public static class ValidationResultExtensions
+    {
+        public static ValidationResult AddCustomError(this ValidationResult validationResult, string errorMessage)
+        {
+            validationResult.Errors.Add(new ValidationFailure(string.Empty, errorMessage));
+            return validationResult;
+        }
+    }
+}
