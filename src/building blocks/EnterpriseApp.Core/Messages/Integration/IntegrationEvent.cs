3f560efdda8f3973f0f2cedeb872835db469c1a6
Tiago Pala <tiago_pala@outlook.com>
2022-01-19 08:20:30 -0300

    Criação de um IntegrationEvent e ResponseMessage para trabalhar com o RequestResponse Pattern do RabbitMQ

---
+++ src/building blocks/EnterpriseApp.Core/Messages/Integration/IntegrationEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EnterpriseApp.Core.Messages.Integration
+{
+    public abstract class IntegrationEvent : Event
+    {
+
+    }
+
+    public class UserRegisteredIntegrationEvent : IntegrationEvent
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public UserRegisteredIntegrationEvent(Guid id, string name, string email, string cpf)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
