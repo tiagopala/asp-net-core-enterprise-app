071cb341aac0f339254a6e40c0e35e0161dbff1f
Tiago Pala <tiago_pala@outlook.com>
2022-01-26 14:30:43 -0300

    Implementação base da API de carrinho de compras

---
+++ src/services/EnterpriseApp.Carrinho.API/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EnterpriseApp.Carrinho.API.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ src/services/EnterpriseApp.Carrinho.API/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EnterpriseApp.Carrinho.API.Controllers
+{
+    [Route("[controller]")]
+    public class CartController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
