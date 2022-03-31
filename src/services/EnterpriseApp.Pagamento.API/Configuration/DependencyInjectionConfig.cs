d642b863d43620e3e9160a5829417070d566fc4e
Tiago Pala <tiago_pala@outlook.com>
2022-03-31 08:51:31 -0300

    Início da criação da API de pagamentos (payments) e inclusão de uma class library que irá simular nosso gateway externo de pagamentos

---
+++ src/services/EnterpriseApp.Pagamento.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,20 @@
+﻿using EnterpriseApp.Core.Services;
+using EnterpriseApp.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EnterpriseApp.Cliente.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection AddAppServices(this IServiceCollection services)
+        {
+            #region Services
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUserService, UserService>();
+            #endregion
+
+            return services;
+        }
+    }
+}
---
+++ src/services/EnterpriseApp.Pagamento.API/Controllers/PaymentsController.cs
@@ -0,0 +1,13 @@
+﻿using EnterpriseApp.API.Core.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseApp.Pagamento.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class PaymentsController : MainController
+    {
+        [HttpGet]
+        public IActionResult Get()
+            => Ok();
+    }
+}
