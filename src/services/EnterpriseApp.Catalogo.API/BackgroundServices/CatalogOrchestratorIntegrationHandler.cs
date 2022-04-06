b2cc266d0289a59bbd40027e2550ee4dd601dd11
Tiago Pala <tiago_pala@outlook.com>
2022-04-06 20:46:06 -0300

    Fix exception message

--- src/services/EnterpriseApp.Catalogo.API/BackgroundServices/CatalogOrchestratorIntegrationHandler.cs
+++ src/services/EnterpriseApp.Catalogo.API/BackgroundServices/CatalogOrchestratorIntegrationHandler.cs
@@ -68,7 +68,7 @@ namespace EnterpriseApp.Catalogo.API.BackgroundServices
             if (!await productRepository.UnitOfWork.Commit())
             {
                 // Disclaire: Caso a exceção seja lançada, a mensagem será enviada para uma fila de erros a qual tentará ser reprocessada até ser processada com sucesso.
-                throw new DomainException($"Problems happned whilte updating database for OrderId:{@event.OrderId}.");
+                throw new DomainException($"Problems happened while updating database for OrderId:{@event.OrderId}.");
             }
 
             var withdrawnOrder = new WithdrawnOrderIntegrationEvent(@event.CustomerId, @event.OrderId);
